ccefe0faa24f52cfe7e655d9e4720eb6b46cd000
LonelyRandom <lonelygrowtopian@gmail.com>
2026-03-07 12:44:07 +0700

    fix detailed

--- StreamlitApp/user_1.py
+++ StreamlitApp/user_1.py
@@ -331,7 +331,7 @@ def display_film_card(df, tag_df):
         img_card_height = 215
         img_card_width = 115
     else:
-        img_card_height = 201
+        img_card_height = 202
         img_card_width = 106
     
     if search_name:
