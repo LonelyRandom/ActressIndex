14be49b9696c59406cdbf549be7ad4784d3612d9
LonelyRandom <lonelygrowtopian@gmail.com>
2026-03-08 09:55:38 +0700

    fix values handling

--- StreamlitApp/app.py
+++ StreamlitApp/app.py
@@ -11,12 +11,10 @@ user_3 = st.secrets.indicators.USER_3
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 if 'page' not in st.session_state:
-    # st.session_state.page = 'login'
-    st.session_state.page = 'home'
+    st.session_state.page = 'login'
 
 if 'usn' not in st.session_state:
-    # st.session_state.usn = None
-    st.session_state.usn = 'zedkari'
+    st.session_state.usn = None
 
 if 'check_login' not in st.session_state:
     st.session_state.check_login = None
