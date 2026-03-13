350704ffdf4b9483b1ef722b453e18343edc8226
LonelyRandom <lonelygrowtopian@gmail.com>
2026-03-13 19:06:41 +0700

    fix actress edit film

--- StreamlitApp/user_1.py
+++ StreamlitApp/user_1.py
@@ -2707,8 +2707,8 @@ def complex_actress(conn, device):
         pos = st.session_state.position
         film = film_df.iloc[pos]
         with st.container(horizontal=True):
-            st.button('⬅️ Previous Film', width='stretch', on_click=set_actress_film_page, args=(st.session_state.position-1,film_df.index[st.session_state.position-1]), disabled=(st.session_state.position == 0))
-            st.button('➡️ Next Film', width='stretch', on_click=set_actress_film_page, args=(st.session_state.position+1,film_df.index[st.session_state.position+1]), disabled=(st.session_state.position == total_film-1))
+            st.button('⬅️ Previous Film', width='stretch', on_click=set_actress_film_page, args=(st.session_state.position-1,film_df.index[st.session_state.position]), disabled=(st.session_state.position == 0))
+            st.button('➡️ Next Film', width='stretch', on_click=set_actress_film_page, args=(st.session_state.position+1,film_df.index[st.session_state.position]), disabled=(st.session_state.position == int(total_film)-1))
         def reset_pic():
             st.session_state.prev_pic = 0
 
