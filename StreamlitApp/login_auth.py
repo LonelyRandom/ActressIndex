0d09d0147aa1a6cef7e0bf3da9a18428508cae67
LonelyRandom <lonelygrowtopian@gmail.com>
2026-04-19 21:23:14 +0700

    Update login_auth.py

--- StreamlitApp/login_auth.py
+++ StreamlitApp/login_auth.py
@@ -66,7 +66,6 @@ def log_in(conn):
         
         if login_button:
             pass_hash = hash_password(username+password)
-            st.write(pass_hash)
             st.session_state.login_error = None
             
             user = st.session_state.login_data[
