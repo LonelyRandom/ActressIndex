6e6b101589233f0a8454bff9d7731803e61f1f37
LonelyRandom <lonelygrowtopian@gmail.com>
2026-05-14 23:39:15 +0700

    Update user_1.py

--- StreamlitApp/user_1.py
+++ StreamlitApp/user_1.py
@@ -751,10 +751,10 @@ def display_film_calender(df):
         st.session_state.show_date = date.today()
     if 'date_clicked' not in st.session_state:
         st.session_state.date_clicked = False
-    selected_flag = st.selectbox('Flag', options=['All', 'Pass','Drop', 'Not Checked', 'Unsure'], width='stretch', on_change=reset_calender_page, key='calender_flag')
+    selected_flag = st.selectbox('Flag', options=['🔵 All', '🟢 Pass','🔴 Drop', '⚪️ Not Checked', '😔 Unsure'], width='stretch', on_change=reset_calender_page, key='calender_flag')
 
-    if selected_flag != 'All':
-        filtered_df = filtered_df[filtered_df['Flag'] == selected_flag]
+    if selected_flag[2:] != 'All':
+        filtered_df = filtered_df[filtered_df['Flag'] == selected_flag[2:]]
 
     def set_month_year(p):
         st.session_state.selected_date = p
