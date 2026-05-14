bd42aa196ae3537bd22bea64714fdd0c801b862b
LonelyRandom <lonelygrowtopian@gmail.com>
2026-05-15 06:30:08 +0700

    Update user_1.py

--- StreamlitApp/user_1.py
+++ StreamlitApp/user_1.py
@@ -754,6 +754,8 @@ def display_film_calender(df):
     selected_flag = st.selectbox('Flag', options=['🔵 All', '🟢 Pass','🔴 Drop', '⚪️ Not Checked', '😔 Unsure'], width='stretch', on_change=reset_calender_page, key='calender_flag')
     st.write(selected_flag)
     st.write(selected_flag[2:])
+    if selected_flag[2:] == 'Not Checked':
+        st.write('ini not checked')
     if selected_flag[2:] != 'All':
         filtered_df = filtered_df[filtered_df['Flag'] == (selected_flag[2:])]
 
