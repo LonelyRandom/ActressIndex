402fdacda59acc2f9e1316225c9c976376581a9b
LonelyRandom <lonelygrowtopian@gmail.com>
2026-08-15 16:42:43 +0700

    add new scrap mode (multiple)

--- StreamlitApp/user_1.py
+++ StreamlitApp/user_1.py
@@ -2435,7 +2435,7 @@ def display_scrap_manual():
                     date_size = 14
                     film_size = 9
                 else:
-                    image_width = 140
+                    image_width = 168
                     btn_width = 36
                     btn_height = 35
                     date_size = 10
